8baf5bc6c6b9ab048e1cd6295a2109000ba3862b
kushpatel0703 <kushpatel0703@gmail.com>
2019-01-07 17:04:40 -0600

    Game of Life Mechanics Added

--- board.py
+++ board.py
@@ -3,8 +3,55 @@ from random import getrandbits
 class board:
     def __init__(self, size):
         self.board = []
+        self.size = size
 
-        for i in range(size):
+        for i in range(self.size):
             self.board.append([])
-            for j in range(size):
+            for j in range(self.size):
                 self.board[i].append(getrandbits(1))
+
+    def runLifeCycle(self):
+        newBoard = []
+
+        for i in range(self.size):
+            newBoard.append([])
+            for j in range(self.size):
+                newBoard[i].append(self.checkNeighbors(i, j))
+
+        if newBoard != self.board:
+            self.board = newBoard
+
+    def checkNeighbors(self, i, j):
+        liveNeighbors = self.checkNeighborsHelper(i,j)
+
+        if self.board[i][j] == 1:
+            if liveNeighbors == 2 or liveNeighbors == 3:
+                return 1
+            else:
+                return 0
+        else:
+            if liveNeighbors == 3:
+                return 1
+            else:
+                return 0
+
+    def checkNeighborsHelper(self, i, j):
+        liveNeighbors = 0
+
+        if (i-1) > 0 and (i-1) < (self.size - 1):
+            if self.board[i-1][j] == 1:
+                liveNeighbors += 1
+
+        if (i+1) > 0 and (i+1) < (self.size - 1):
+            if self.board[i+1][j] == 1:
+                liveNeighbors += 1
+
+        if (j-1) > 0 and (j-1) < (self.size - 1):
+            if self.board[i][j-1] == 1:
+                liveNeighbors += 1
+
+        if (j+1) > 0 and (j+1) < (self.size - 1):
+            if self.board[i][j+1] == 1:
+                liveNeighbors += 1
+
+        return liveNeighbors
--- main.py
+++ main.py
@@ -4,5 +4,11 @@ def main():
     gameBoard = board(4)
     print(gameBoard.board)
 
+    for _ in range(100):
+        gameBoard.runLifeCycle()
+        print(gameBoard.board)
+
+
+
 if __name__ == "__main__":
     main()
